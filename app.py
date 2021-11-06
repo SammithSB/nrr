0225dff2bb473f2fe2f4f7515d755f0fdec91ede
SammithSB <sammithsb@gmail.com>
2021-11-06 08:27:06 +0530

    updated india game

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ runs_scored= 110 + 151 + 210
 overs_faced =60
 runs_conceeded=111 + 152 + 144
 overs_bowled=14.5+(17+(5.0/6))+20
-input_runs_scored = int(input("enter runs scored by India: "))
-input_overs_faced = int(input("enter overs faced(if it is 17.1 type 17+(1/6)) by India: "))
-input_runs_conceeded = int(input("enter runs conceeded by India: "))
-input_oves_bowled = int(input("enter overs bowled(if it is 17.1 type 17+(1/6)) by India: "))
-nrr = ((runs_scored+input_runs_scored)/(overs_faced+input_overs_faced)) - ((runs_conceeded+input_runs_conceeded)/(overs_bowled+input_oves_bowled))
-print(nrr)
+input_runs_scored =89
+input_overs_faced = 6.5
+input_runs_conceeded =85
+input_oves_bowled = 20
+nrr_ind = ((runs_scored+input_runs_scored)/(overs_faced+input_overs_faced)) - ((runs_conceeded+input_runs_conceeded)/(overs_bowled+input_oves_bowled))
+nrr_afg = 8.49-((516+130)/99.0)
+print(nrr_ind)
+print(nrr_afg)
